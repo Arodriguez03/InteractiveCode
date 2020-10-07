06c1296e813e89f6a1eaf38a225c93be841598e5
Alejandro Rodriguez <alejandro.rodriguez@patelhs.org>
2020-10-07 17:21:46 +0000

    Added a new scanner code

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-  
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Hi, could you print your name?");
+      String name = scan.nextLine();
+
+      System.out.print("Hello " + name + "!");
+      
+      System.out.println(" How old are you " + name + "?");
+      int age = scan.nextInt();
+      if (age <= 15){
+        
+        System.out.println("Wow, you are young!");
+      
+          }else if (age >= 15){
+            System.out.println("You are getting old!");
+      }
+
+     
   }
 }
